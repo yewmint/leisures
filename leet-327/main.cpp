136a13944a01080d1edac3fa160e6be8815cd04a
yewmint <yewmint@outlook.com>
2018-03-15 16:15:04 +0800

    Add 3 more problems

--- leet-327/main.cpp
+++ leet-327/main.cpp
@@ -13,7 +13,7 @@
 #include <cmath>
 #include <functional>
 #include <climits>
-#include "fenwick.h"
+#include "binary-search.h"
 
 using namespace std;
 using namespace yewmint;
@@ -33,9 +33,38 @@ struct TreeNode {
 };
 
 #define mp make_pair
+typedef long long ll;
+
+int countSubRange(vector<int>& nums, int lower, int upper, int l, int r) {
+	if (l == r) {
+		return nums[l] >= lower && nums[l] <= upper ? 1 : 0;
+	}
+
+	int m = (l + r) / 2;
+	vector<ll> sums(r - m, 0);
+	ll sum = 0;
+	for (int i = m + 1; i <= r; ++i) {
+		sum += nums[i];
+		sums[i - m - 1] = sum;
+	}
+
+	sort(sums.begin(), sums.end());
+
+	sum = 0;
+	int cnt = 0;
+	for (int i = m; i >= l; --i) {
+		sum += nums[i];
+		cnt += binarySearchUpper(sums, upper - sum + 0.5) - binarySearchUpper(sums, lower - sum - 0.5);
+	}
+
+	return countSubRange(nums, lower, upper, l, m) + countSubRange(nums, lower, upper, m + 1, r) + cnt;
+}
 
 int countRangeSum(vector<int>& nums, int lower, int upper) {
-	Fenwick<int> fw(nums);
+	if (nums.empty()) {
+		return 0;
+	}
+	return countSubRange(nums, lower, upper, 0, nums.size() - 1);
 }
 
 int main() {
