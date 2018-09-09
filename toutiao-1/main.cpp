0ef9238a95fc6e8490e9368c258a9adbc4926f1a
yewmint <yewmint@outlook.com>
2018-09-09 09:58:35 +0800

    Add toutiao exam 1

---
+++ toutiao-1/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define mp make_pair
+typedef pair<int, int> pii;
+typedef long long ll;
+
+template<typename T>
+ll scll(T val){
+  return static_cast<ll>(val);
+}
+
+int n, k, t;
+
+int main() {
+  scanf("%d%d%d", &n, &k, &t);
+  printf("%d", ret);
+}
